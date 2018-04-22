8c747d5400cf40437ea3746ed4e7692e29c79734
Balázs Polgár <balage06@gmail.com>
2018-04-22 22:59:36 +0200

    Update AppDatabase

--- app/src/main/java/hu/balpo/rategp/datastore/AppDatabase.java
+++ app/src/main/java/hu/balpo/rategp/datastore/AppDatabase.java
@@ -1,5 +1,23 @@
 package hu.balpo.rategp.datastore;
 
-public class AppDatabase {
-    //TODO: Room database implementation for local DB storage
+import java.util.List;
+
+import hu.balpo.rategp.datastore.entity.Event;
+import hu.balpo.rategp.datastore.entity.Review;
+import hu.balpo.rategp.datastore.entity.Serie;
+
+public final class AppDatabase {
+    private AppDatabase(){}
+
+    public static List<Serie> getAllSeries(){
+        return Serie.listAll(Serie.class);
+    }
+
+    public static List<Event> getAllEventsBySerie(Serie serie){
+        return Event.find(Event.class, "serie = ?", String.valueOf(serie.getId()));
+    }
+
+    public static List<Review> getAllReviesByEvent(Event event){
+        return Review.find(Review.class, "event = ?", String.valueOf(event.getId()));
+    }
 }
